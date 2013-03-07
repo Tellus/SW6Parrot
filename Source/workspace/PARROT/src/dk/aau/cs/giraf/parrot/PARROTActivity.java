5211646ffc68ef748a9ab3cdbb4466bea7d654da
Lisbeth Nielsen <lniel1989@hotmail.com>
2013-03-07 15:47:46 +0100

    kan logge ind med tom bruger uden den crasher

--- Source/workspace/PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
+++ Source/workspace/PARROT/src/dk/aau/cs/giraf/parrot/PARROTActivity.java
@@ -20,72 +20,76 @@ public class PARROTActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		//These lines get the intent from the launcher //TODO use us when testing with the launcher.
-		/*girafIntent = getIntent();
-		Helper help = new Helper(this);
-		app = help.appsHelper.getAppByPackageName();
-		guardianID = girafIntent.getExtras().getLong("currentGuardianID");*/
-		
 		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.main);
-
-
-
-		dataLoader = new PARROTDataLoader(this);
-		//parrotUser = dataLoader.loadPARROT();			
-		//TODO replace the temp lines with the above line
-		//START TEMP LINES
-		dataLoader.TESTsaveTestProfile();
-		parrotUser.setRights(0, true);
-		parrotUser.setRights(1, true);
-		parrotUser.setRights(2, true);
-		//END TEMP LINES
-
-		/* Here all the Tabs in the system is initialized based on whether or not a user
-		 * is allowed to use them. If not they will not be initialized.
-		 * We wish not make users aware that there exists functionality that they are not
-		 * entitled to.
-		 * Remember: Make sure the order of the Taps is consistent with the order of their rights in the
-		 * 			 Rights array.
-		 */
+		//These lines get the intent from the launcher //TODO use us when testing with the launcher.
+		girafIntent = getIntent();
+		Helper help = new Helper(this);
+		app = help.appsHelper.getAppByPackageName();
+		guardianID = girafIntent.getLongExtra("currentGuardianID", -1);
 		
-		ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.setDisplayShowTitleEnabled(false);	//TODO figure out what this does
-
-		//Creating a new Tab, setting the text it is to show and construct and attach a Tab Listener to control it.
-		Tab tab = actionBar.newTab() 
-				.setText(R.string.firstTab)
-				.setTabListener(new TabListener<SpeechBoardFragment>(this,"speechboard",SpeechBoardFragment.class));
-		actionBar.addTab(tab);
-		
-		if (parrotUser.getRights(0) == true)
+		if(guardianID == -1 )
 		{
-			tab = actionBar.newTab()
-					.setText(R.string.secondTab)
-					.setTabListener(new TabListener<ManageCategoryFragment>(this,"manage",ManageCategoryFragment.class));
-			actionBar.addTab(tab);
+			//do nothing
 		}
-		if (parrotUser.getRights(1) == true) 
+		else
 		{
-			tab = actionBar.newTab()
-					.setText(R.string.thirdTab)
-					.setTabListener(new TabListener<OptionsFragment>(this,"options",OptionsFragment.class));
+			dataLoader = new PARROTDataLoader(this);
+			parrotUser = dataLoader.loadPARROT();			
+			//TODO replace the temp lines with the above line
+			//START TEMP LINES
+			/*dataLoader.TESTsaveTestProfile();
+			parrotUser.setRights(0, true);
+			parrotUser.setRights(1, true);
+			parrotUser.setRights(2, true);*/
+			//END TEMP LINES
+	
+			/* Here all the Tabs in the system is initialized based on whether or not a user
+			 * is allowed to use them. If not they will not be initialized.
+			 * We wish not make users aware that there exists functionality that they are not
+			 * entitled to.
+			 * Remember: Make sure the order of the Taps is consistent with the order of their rights in the
+			 * 			 Rights array.
+			 */
+			
+			ActionBar actionBar = getActionBar();
+			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+			actionBar.setDisplayShowTitleEnabled(false);	//TODO figure out what this does
+	
+			//Creating a new Tab, setting the text it is to show and construct and attach a Tab Listener to control it.
+			Tab tab = actionBar.newTab() 
+					.setText(R.string.firstTab)
+					.setTabListener(new TabListener<SpeechBoardFragment>(this,"speechboard",SpeechBoardFragment.class));
 			actionBar.addTab(tab);
+			
+			if (parrotUser.getRights(0) == true)
+			{
+				tab = actionBar.newTab()
+						.setText(R.string.secondTab)
+						.setTabListener(new TabListener<ManageCategoryFragment>(this,"manage",ManageCategoryFragment.class));
+				actionBar.addTab(tab);
+			}
+			if (parrotUser.getRights(1) == true) 
+			{
+				tab = actionBar.newTab()
+						.setText(R.string.thirdTab)
+						.setTabListener(new TabListener<OptionsFragment>(this,"options",OptionsFragment.class));
+				actionBar.addTab(tab);
+			}
 		}
 
 
 	}
 	@Override
 	protected void onPause() {
-		AudioPlayer.close();
-		dataLoader.saveProfile(getUser());
+		/*AudioPlayer.close();
+		dataLoader.saveProfile(getUser());*/
 		super.onPause();
 	}
 
 	@Override
 	protected void onResume() {
-		AudioPlayer.open();
+		//AudioPlayer.open();
 		super.onResume();
 	}
 
