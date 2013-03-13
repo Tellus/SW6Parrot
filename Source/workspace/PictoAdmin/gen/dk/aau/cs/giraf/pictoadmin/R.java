c8b2a00c4d5f12934f5e1e375955ea2289293394
Christian Klim Hansen <christian.klim.hansen@gmail.com>
2013-03-13 15:52:31 +0100

    Changed GUI

--- Source/workspace/PictoAdmin/gen/dk/aau/cs/giraf/pictoadmin/R.java
+++ Source/workspace/PictoAdmin/gen/dk/aau/cs/giraf/pictoadmin/R.java
@@ -22,15 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int testbackground1=0x7f020001;
     }
     public static final class id {
         public static final int Kategori=0x7f080004;
         public static final int Title=0x7f080006;
         public static final int action_settings=0x7f080007;
         public static final int editText1=0x7f080000;
-        public static final int gridView1=0x7f080001;
         public static final int search_button=0x7f080003;
+        public static final int searched_pictograms=0x7f080001;
         public static final int text_input=0x7f080002;
         public static final int underKategori=0x7f080005;
     }
