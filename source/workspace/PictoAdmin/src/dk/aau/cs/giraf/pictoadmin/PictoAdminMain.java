6623b1ac0ad0c0e5579983557b5b55b640921b6f
Christian Klim Hansen <christian.klim.hansen@gmail.com>
2013-03-26 19:26:49 +0100

    testing changes

--- source/workspace/PictoAdmin/src/dk/aau/cs/giraf/pictoadmin/PictoAdminMain.java
+++ source/workspace/PictoAdmin/src/dk/aau/cs/giraf/pictoadmin/PictoAdminMain.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,29 +22,33 @@ public class PictoAdminMain extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_picto_admin_main);
 		
-		inputbox = (EditText) findViewById(R.id.text_input);
+		//inputbox = (EditText) findViewById(R.id.text_input);
 		
 		//Setting up the button such that it may be clicked
-		searchbutton.findViewById(R.id.search_button);
-		searchbutton.setOnClickListener(searchButtonHandler);
+		//searchbutton.findViewById(R.id.search_button);
+		//searchbutton.setOnClickListener(searchButtonHandler);
 		
 	}
 
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.activity_picto_admin_main);
+		super.onResume();
+	}
 	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.picto_admin_main, menu);
 		
-		//Setting up the button such that it may be clicked
-		searchButton.findViewById(R.id.search_button);
-		searchButton.setOnClickListener(searchButtonHandler);
-		
 		return true;
 	}*/
+
 	
 	
 	// Button event handlers
-	View.OnClickListener searchButtonHandler = new View.OnClickListener() {
+	/*View.OnClickListener searchButtonHandler = new View.OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
@@ -54,5 +59,5 @@ public class PictoAdminMain extends Activity {
 			
 			//disphandler.updatePictogramDisplayer(pictograms);
 		}
-	};
+	};*/
 }
