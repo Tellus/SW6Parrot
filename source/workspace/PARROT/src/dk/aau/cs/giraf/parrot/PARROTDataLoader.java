83af00bf168a6f6483a427ea2ab8ce9bd016fd02
Lisbeth Nielsen <lniel1989@hotmail.com>
2013-04-04 11:31:53 +0200

    nu uden testData filen

--- source/workspace/PARROT/src/dk/aau/cs/giraf/parrot/PARROTDataLoader.java
+++ source/workspace/PARROT/src/dk/aau/cs/giraf/parrot/PARROTDataLoader.java
@@ -103,30 +103,7 @@ public class PARROTDataLoader {
 			}
 			
 			return parrotUser;
-			/* if(specialSettings.containsKey("category0"))
-			{
-				Log.v("MessageParrot", "ind categori");	
-				for(int number = 0; specialSettings.get("category"+number) != null; number++)
-				{
-					categoryString = specialSettings.get("category"+number).get("pictograms");
-					String colorString = specialSettings.get("category"+number).get("colour");
-					int col=Integer.valueOf(colorString);
-					String iconString = specialSettings.get("category"+number).get("icon");
-					String catName = specialSettings.get("category"+number).get("name");
-					parrotUser.addCategory(loadCategory(catName,categoryString,col,iconString));					
-				}
-				return parrotUser;
-			}
-			else
-			{	
-				AlertDialog alertDialog;
-				alertDialog = new AlertDialog.Builder(parent).create();
-				alertDialog.setTitle("Error");
-				alertDialog.setMessage("Child has no categories");
-				alertDialog.show();
-				return null;
-			}
-			*/
+			
 		}
 		//If an error has happened, return null
 		AlertDialog alertDialog;
@@ -138,115 +115,39 @@ public class PARROTDataLoader {
 	}
 
 	private PARROTProfile loadSettings(PARROTProfile parrotUser, Setting<String, String, String> profileSettings) {
-
 		/*new settings*/
-		int noOfBoxes = Integer.valueOf(profileSettings.get("SentenceboardSettings").get("NoOfBoxes"));
-		boolean showText = Boolean.valueOf(profileSettings.get("PictogramSettings").get("ShowText"));
-		String PictogramSize = String.valueOf(profileSettings.get("PictogramSettings").get("PictogramSize"));
-		int sentenceColour = Integer.valueOf(profileSettings.get("SentenceboardSettings").get("Color"));	
-		
-		parrotUser.setSentenceBoardColor(sentenceColour);
-		parrotUser.setNumberOfSentencePictograms(noOfBoxes);
-		if(PictogramSize.equalsIgnoreCase("MEDIUM"))
-		{ 
-			parrotUser.setPictogramSize(PARROTProfile.PictogramSize.MEDIUM);
+		//if(profileSettings.containsKey(SentenceboardSettings))
+		Log.v("PARROTMessAGE", "BEFORE get from settings");
+		try{
+			int noOfBoxes = Integer.valueOf(profileSettings.get("SentenceboardSettings").get("NoOfBoxes"));
+			boolean showText = Boolean.valueOf(profileSettings.get("PictogramSettings").get("ShowText"));
+			String PictogramSize = String.valueOf(profileSettings.get("PictogramSettings").get("PictogramSize"));
+			int sentenceColour = Integer.valueOf(profileSettings.get("SentenceboardSettings").get("Color"));	
+			
+			parrotUser.setSentenceBoardColor(sentenceColour);
+			parrotUser.setNumberOfSentencePictograms(noOfBoxes);
+			if(PictogramSize.equalsIgnoreCase("MEDIUM"))
+			{ 
+				parrotUser.setPictogramSize(PARROTProfile.PictogramSize.MEDIUM);
+			}
+			else if(PictogramSize.equalsIgnoreCase("LARGE"))
+			{
+				parrotUser.setPictogramSize(PARROTProfile.PictogramSize.LARGE);
+			}
+			
+			if(showText)
+			{
+				parrotUser.setShowText(true);
+			}
 		}
-		else if(PictogramSize.equalsIgnoreCase("LARGE"))
+		catch(Exception e)
 		{
-			parrotUser.setPictogramSize(PARROTProfile.PictogramSize.LARGE);
+			
 		}
-		
-		if(showText)
-		{
-			parrotUser.setShowText(true);
-		}
-		
 		
 		return parrotUser;
 	}
 	
-	//This method loads category
-	/*public PARROTCategory loadCategory(String catName, String pictureIDs,int colour,String iconString)
-	{
-		Long iconId = Long.valueOf(iconString);
-		PARROTCategory cat = new PARROTCategory(catName, colour, loadPictogram(iconId));
-		ArrayList<Long> listIDs = getIDsFromString(pictureIDs);
-		for(int i = 0; i<listIDs.size();i++)
-		{
-			cat.addPictogram(loadPictogram(listIDs.get(i)));
-		}
-		return cat;
-	}*/
-
-	/*
-	public PictogramOLD loadPictogram(long idPictogram)
-	{
-		PictogramOLD pic = null;
-		Media media=help.mediaHelper.getSingleMediaById(idPictogram); //This is the image media //TODO check type
-
-		List<Media> subMedias =	help.mediaHelper.getSubMediaByMedia(media); 
-		String soundPath = null;
-		String wordPath = null;
-		long soundID = -1; //If this value is still -1 when we save a media, it is because the pictogram has no sound.
-		long wordID = -1;
-		
-		if(subMedias != null)	//Media files can have a link to a sub-media file, check if this one does.
-		{	
-			Media investigatedMedia;
-			for(int i = 0; i<subMedias.size();i++) 		
-			{
-				investigatedMedia = subMedias.get(i);
-				if(investigatedMedia.getMType().equals("SOUND"))
-				{
-					soundPath = investigatedMedia.getMPath();
-					soundID= investigatedMedia.getId();
-				}
-				else if(investigatedMedia.getMType().equals("WORD"))
-				{
-					wordPath = investigatedMedia.getMPath();
-					wordID = investigatedMedia.getId();
-				}
-			}
-		}
-		pic = new PictogramOLD(media.getName(), media.getMPath(), soundPath, wordPath);
-		//set the different ID's
-		pic.setImageID(idPictogram);
-		pic.setSoundID(soundID);
-		pic.setWordID(wordID);
-
-		return pic;
-	}
-
-	//This function takes a string consisting of IDs, and returns a list of integer IDs instead
-	public ArrayList<Long> getIDsFromString(String IDstring)
-	{
-		ArrayList<Long> listOfID = new ArrayList<Long>();
-
-		if(IDstring !=null && IDstring.charAt(0)!='$' && IDstring.charAt(0)!='#')
-		{
-			String temp = String.valueOf(IDstring.charAt(0));
-			for(int w = 1; IDstring.charAt(w)!='$'; w++)
-			{
-				if(IDstring.charAt(w)!='#')
-				{
-					temp = temp+ IDstring.charAt(w);
-				}
-				else if(IDstring.charAt(w)=='$')
-				{
-					break;
-				}
-				else
-				{
-					listOfID.add(Long.valueOf(temp));
-					w++;
-					temp = String.valueOf(IDstring.charAt(w));
-				}
-			}
-		}
-
-		return listOfID;
-	}
-*/
 	public void saveChanges(PARROTProfile user)
 	{
 		Profile prof = help.profilesHelper.getProfileById(user.getProfileID());
