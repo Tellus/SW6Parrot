3a057f91eb2118afde972ab90ee37a17a8d96e07
Anders Vinther <anders.vinther1@gmail.com>
2013-04-06 16:44:47 +0200

    Random

--- source/workspace/PictoAdmin/gen/com/example/categorymanager/R.java
+++ source/workspace/PictoAdmin/gen/com/example/categorymanager/R.java
@@ -19,7 +19,7 @@ public final class R {
 		public static final int main = 0x7f030003;
 	}
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f02000b;
-		public static final int usynlig = 0x7f020014;
+		public static final int ic_launcher = 0x7f02000c;
+		public static final int usynlig = 0x7f020011;
 	}
 }
