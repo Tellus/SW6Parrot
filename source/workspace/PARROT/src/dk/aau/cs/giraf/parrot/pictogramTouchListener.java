1be6ae282105f3a6a6647229d296deb0eb0cf082
Lisbeth Nielsen <lniel1989@hotmail.com>
2013-04-16 16:58:58 +0200

    en version der virker

---
+++ source/workspace/PARROT/src/dk/aau/cs/giraf/parrot/pictogramTouchListener.java
@@ -0,0 +1,37 @@
+package dk.aau.cs.giraf.parrot;
+
+import android.content.ClipData;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnTouchListener;
+
+public class pictogramTouchListener implements OnTouchListener {
+	private int position;
+
+	public pictogramTouchListener(int position)
+	{
+		this.position=position;
+	}
+	@Override
+	public boolean onTouch(View view, MotionEvent event) {
+
+		if (event.getAction() == MotionEvent.ACTION_DOWN) { 
+		     
+			SpeechBoardFragment.draggedPictogramIndex = position;
+			
+			ClipData data = ClipData.newPlainText("label", "text");
+	    	DragShadowBuilder shadowBuilder = new DragShadowBuilder(view);
+	    	boolean startedDragging=view.startDrag(data, shadowBuilder, view, 0);
+	    	//Log.v("DraggingMessage", "startedDragging: "+ startedDragging);
+	        return true;
+			
+		 }
+		 else if (event.getAction() == MotionEvent.ACTION_DOWN) { 
+		       
+		    }
+	    
+	    return false;
+	}
+
+}
