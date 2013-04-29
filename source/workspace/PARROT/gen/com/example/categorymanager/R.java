ec085353d20059c1e9799b4267d4fa5c01e4ddfc
Lisbeth Nielsen <lniel1989@hotmail.com>
2013-04-29 14:49:20 +0200

    s

--- source/workspace/PARROT/gen/com/example/categorymanager/R.java
+++ source/workspace/PARROT/gen/com/example/categorymanager/R.java
@@ -16,10 +16,24 @@ public final class R {
 		public static final int app_name = 0x7f040001;
 	}
 	public static final class layout {
+		public static final int picto_layout = 0x7f030004;
 		public static final int main = 0x7f030003;
+	}
+	public static final class styleable {
+		public static final int Pictogram_width = 0;
+		public static final int[] Pictogram = { 0x7f010000, 0x7f010001 };
+		public static final int Pictogram_height = 1;
 	}
 	public static final class drawable {
 		public static final int ic_launcher = 0x7f02000b;
 		public static final int usynlig = 0x7f020016;
+	}
+	public static final class attr {
+		public static final int height = 0x7f010001;
+		public static final int width = 0x7f010000;
+	}
+	public static final class dimen {
+		public static final int activity_vertical_margin = 0x7f050001;
+		public static final int activity_horizontal_margin = 0x7f050000;
 	}
 }
